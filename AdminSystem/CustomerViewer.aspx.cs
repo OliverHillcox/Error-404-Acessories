7f5fdd30867f5533ce13cb49fac468631333c103
OliverHillcox <oliverhillcox@gmail.com>
2022-02-16 15:41:53 +0000

    Did Week up to week 21 Work

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsCustomer ACustomer = new clsCustomer();
+
+        ACustomer = (clsCustomer)Session["ACustomer"];
+
+        Response.Write(ACustomer.Name);
+        Response.Write("<br/>");
+        Response.Write(ACustomer.EmailAddress);
+        Response.Write("<br/>");
+        Response.Write(ACustomer.Address);
+        Response.Write("<br/>");
+        Response.Write(ACustomer.PostCode);
+        Response.Write("<br/>");
+        Response.Write(ACustomer.Birthday.ToString().Split(' ')[0]);
+        Response.Write("<br/>");
+        Response.Write(ACustomer.PhoneNumber);
+        Response.Write("<br/>");
+        Response.Write(ACustomer.Active);
 
     }
 }
--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool Active { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string PostCode { get; set; }
+        public string Address { get; set; }
     }
 }
