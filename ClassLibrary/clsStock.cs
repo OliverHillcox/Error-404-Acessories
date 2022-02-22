14b51019c3c7b372f01fc7feb3fc1009842c7680
Matthew Harvey <P2590750@my365.dmu.ac.uk>
2022-02-22 14:07:25 +0000

    new testing stock class

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStock
+    {
+        public bool ItemOver18 { get; set; }
+        public DateTime ItemDateAdded { get; set; }
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public int ItemQuantity { get; set; }
+    }
+}
---
+++ Testing2/tstStock.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary;
+
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock SomeStock = new clsStock();
+            Assert.IsNotNull(SomeStock);
+        }
+    }
+}
