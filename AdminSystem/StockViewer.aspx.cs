04fca6e247102b4e7552adad053f101389721c82
Matthew Harvey <P2590750@my365.dmu.ac.uk>
2022-02-23 19:04:00 +0000

    Data Entry and Viewer Classes

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsStock someStock = new clsStock();
+        someStock = (clsStock)Session["someStock"];
 
+        Response.Write(someStock.ItemName);
+        Response.Write("<br/>");
+        Response.Write(someStock.ItemPrice);
+        Response.Write("<br/>");
+        Response.Write(someStock.ItemDateAdded.ToString());
+        Response.Write("<br/>");
+        Response.Write(someStock.ItemQuantity);
+        Response.Write("<br/>");
+        Response.Write(someStock.ItemOver18.ToString());
     }
 }
