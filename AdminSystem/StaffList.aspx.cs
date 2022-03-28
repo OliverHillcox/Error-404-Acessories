8b042a836f96203b35fe28c3e83c71ac7031d41f
John <p2588285@my365.dmu.ac.uk>
2022-03-28 21:04:33 +0100

    finished week 26.

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,10 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+        //if this the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayStaffs();
+        }
+
+    }
+
+    void DisplayStaffs()
+    {
+        clsStaffCollection Staffs = new clsStaffCollection();
+        //set the data source to the list of staffs in the collection
+        lstStaffList.DataSource = Staffs.StaffList;
+        //set the name of the primary key
+        lstStaffList.DataValueField = "StaffId";
+        //set the data field to display
+        lstStaffList.DataTextField = "Address";
+        //bind the data to the list
+        lstStaffList.DataBind();
+    }
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
     {
 
     }
