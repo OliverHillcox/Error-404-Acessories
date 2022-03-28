d334aa42784959326c7b6f47acc05a529415145f
Matthew Harvey <P2590750@my365.dmu.ac.uk>
2022-03-28 10:25:48 +0100

    completed week26

--- ClassLibrary/clsStockCollection.cs
+++ ClassLibrary/clsStockCollection.cs
@@ -1,15 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
     public class clsStockCollection
     {
+
+        List<clsStock> mStockList = new List<clsStock>();
+        public List<clsStock> StockList { get { return mStockList; } set { mStockList = value; } }
+        public int Count { get { return mStockList.Count; } set { } }
+        public clsStock ThisStock { get; set; }
+
         public clsStockCollection()
         {
-        }
+            Int32 Index = 0;
 
-        public List<clsStock> StockList { get; set; }
-        public clsStock ThisStock { get; set; }
-        public int Count { get; set; }
+            Int32 RecordCount = 0;
+
+            clsDataConnection DB = new clsDataConnection();
+
+            DB.Execute("sproc_tblStock_SelectAll");
+
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsStock stock = new clsStock();
+
+                stock.ItemID = Convert.ToInt32(DB.DataTable.Rows[Index]["ItemID"]);
+                stock.ItemOver18 = Convert.ToBoolean(DB.DataTable.Rows[Index]["ItemOver18"]);
+                stock.ItemDateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["ItemDateAdded"]);
+                stock.ItemName = Convert.ToString(DB.DataTable.Rows[Index]["ItemName"]);
+                stock.ItemPrice = Convert.ToDouble(DB.DataTable.Rows[Index]["ItemPrice"]);
+                stock.ItemQuantity = Convert.ToInt32(DB.DataTable.Rows[Index]["ItemQuantity"]);
+
+                mStockList.Add(stock);
+
+                Index++;
+
+            }
+        }
     }
 }
