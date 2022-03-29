41c7c7c117b095d4438c11ce98bceb76f2b62057
Johnchristy Amalathasan <P2588285@my365.dmu.ac.uk>
2022-03-29 18:22:51 +0100

    week 26 part2

--- Testing3/tstStaffCollection.cs
+++ Testing3/tstStaffCollection.cs
@@ -45,19 +45,6 @@ namespace Test_Framework
             Assert.AreEqual(AllStaff.StaffList, TestList);
         }
 
-        /*
-        [TestMethod]
-        public void CountPropertyOK()
-        {
-            //create an instance of the class we want to create
-            clsStaffCollection AllStaff = new clsStaffCollection();
-            //create some test data to assign to the property
-            Int32 SomeCount = 2;
-            //assign the data to the property
-            AllStaff.Count = SomeCount;
-            //test to see that the two values are the same
-            Assert.AreEqual(AllStaff.Count, SomeCount);
-        } */
 
         [TestMethod]
         public void ThisStaffPropertyOK()
@@ -109,15 +96,7 @@ namespace Test_Framework
 
         }
 
-        /*
-        [TestMethod]
-        public void TwoRecordsPresent()
-        {
-            clsStaffCollection AllStaff = new clsStaffCollection();
 
-            //test
-            Assert.AreEqual(AllStaff.Count, 2); 
-        } */
 
     }
 }
